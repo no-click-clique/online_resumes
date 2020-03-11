755c77cb0e56f6290d751f0ca8135417c5615f3e
mike <mikemcfaddin@gmail.com>
2020-03-11 15:28:36 -0400

    starts to adjust the bootstrap theme for show view

--- src/views/StudentsShow.vue
+++ src/views/StudentsShow.vue
@@ -31,6 +31,27 @@
           Screenshot: <span>{{ capstone.screenshot }}</span><br>
         </div>
       </div>
+
+      <section class="page-section cta">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-9 mx-auto">
+              <div class="cta-inner text-center rounded">
+                <h2 class="section-heading mb-4">
+                  <!-- <span class="section-heading-upper">{{ student.email }}</span> -->
+                  <span class="section-heading-lower">{{ student.first_name }} {{ student.last_name }}</span>
+                  <span class="section-heading-upper">Email: {{ student.email }}</span>
+                  <span class="section-heading-upper">Phone: {{ student.phone_number }}</span>
+                  <span class="section-heading-upper">Linkedin: {{ student.linkedin_url }}</span>
+                  <span class="section-heading-upper">Twitter: {{ student.twitter_handle }}</span>
+
+                </h2>
+                <p class="mb-0">When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       
     </div>
    
