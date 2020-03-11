bb0346b6d337e99f27180ea83218426614d919c3
ben <benjamin.n.chapin@gmail.com>
2020-03-11 11:35:59 -0500

    adds student index

--- src/views/StudentsIndex.vue
+++ src/views/StudentsIndex.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="resume-index">
-    <h1>All Resumes</h1>
+  <div class="students-index">
+    <h1>All Students</h1>
     <div v-for="student in students">
-      <h2>{{ student.first_name }}</h2>
+      <h2>{{ student.first_name }} {{ student.last_name }}</h2>
+      <h3><img v-bind:src="student.photo" /></h3>
+      <h3>{{ student.short_bio }}</h3>
+      <router-link v-bind:to="`/students/${student.id}`">More Details</router-link>
     </div>
   </div>
 </template>
@@ -21,7 +24,7 @@ export default {
   },
   created: function() {
     axios.get("api/students").then(response => {
-      this.resumes = response.data;
+      this.students = response.data;
     });
   },
   methods: {}
