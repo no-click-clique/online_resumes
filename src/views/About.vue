6ad64c5f5ea3a0e7d884cd0a4948731c037fb57a
ben <benjamin.n.chapin@gmail.com>
2020-03-12 15:19:22 -0500

    fixes about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This website was created to learn about Actualize students and check out their resumes.</h1>
+    <h1>© COPYRIGHT BEN & MIKE 2020</h1>
   </div>
 </template>
